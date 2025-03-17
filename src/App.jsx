6d356332db56307519a95f18d43e26d9f57611ec
Keshav-goyal-1 <goyalkeshav896@gmail.com>
2025-03-17 07:13:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Square(){
+  return(
+    <div>
+      <button className='px-1 py-0.5 border-1 border-solid border-black'>X</button>
+    </div>
+  )
+}
